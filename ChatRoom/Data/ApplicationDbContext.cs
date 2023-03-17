cf58c8ad3d34cb9b868b101019792fc34fc28864
Eduard-Nicolae <eduard_nicolae@yahoo.com>
2023-03-17 12:22:42 +0200

    Data

---
+++ ChatRoom/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using ChatRoom.Models.DB;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatRoom.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<UserModel>? Users { get; set; }
+
+        public DbSet<GroupModel>? Groups { get; set; }
+
+        public DbSet<RelationshipUserModel>? RelationshipUsers { get; set; }
+
+        public DbSet<RelationshipGroupModel>? RelationshipGroups { get; set; }
+
+        public DbSet<MessageUserModel>? MessageUsers { get; set; }
+
+        public DbSet<MessageGroupModel>? MessageGroups { get; set; }
+    }
+}
