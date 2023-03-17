e2f318c1f2ea33abb95f04905b2a5868f83839cd
Eduard-Nicolae <eduard_nicolae@yahoo.com>
2023-03-17 12:20:18 +0200

    RelationshipGroups table

---
+++ ChatRoomDB/dbo/Tables/RelationshipGroups.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[RelationshipGroups]
+(
+	[IdUser] INT NOT NULL CONSTRAINT FK_RelationshipGroups_User FOREIGN KEY REFERENCES Users(Id),
+	[IdGroup] INT NOT NULL CONSTRAINT FK_RelationshipGroups_Group FOREIGN KEY REFERENCES Groups(Id)
+)
