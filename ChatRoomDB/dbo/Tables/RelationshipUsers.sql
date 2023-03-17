c2ec7b05d1549dcf752cb13d95deddc556ddfc48
Eduard-Nicolae <eduard_nicolae@yahoo.com>
2023-03-17 12:20:30 +0200

    RelationshipUsers table

---
+++ ChatRoomDB/dbo/Tables/RelationshipUsers.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[RelationshipUsers]
+(
+	[IdUserFirst] INT NOT NULL CONSTRAINT FK_RelationshipUsers_UserFirst FOREIGN KEY REFERENCES Users(Id),
+	[IdUserSecond] INT NOT NULL CONSTRAINT FK_RelationshipUsers_UserSecond FOREIGN KEY REFERENCES Users(Id)
+)
