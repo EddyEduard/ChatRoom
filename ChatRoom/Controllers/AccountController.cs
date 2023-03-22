c310a4ba232e7476f81ef04db7bb64a73494709a
Eduard-Nicolae <eduard_nicolae@yahoo.com>
2023-03-22 18:29:35 +0200

    View Controllers

---
+++ ChatRoom/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using ChatRoom.Middleware;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatRoom.Controllers
+{
+    [AuthorizeToken]
+    public class AccountController : Controller
+    {
+        public IActionResult Chat()
+        {
+            return View();
+        }
+    }
+}
