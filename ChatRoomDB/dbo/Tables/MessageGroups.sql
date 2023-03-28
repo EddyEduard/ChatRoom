bf612206821366631b6c8f0f2b96392971949342
Eduard-Nicolae <eduard_nicolae@yahoo.com>
2023-03-28 15:41:53 +0300

    MessageGroups table

--- ChatRoomDB/dbo/Tables/MessageGroups.sql
+++ ChatRoomDB/dbo/Tables/MessageGroups.sql
@@ -4,6 +4,7 @@
 	[IdUser] INT NOT NULL CONSTRAINT FK_MessageGroups_User FOREIGN KEY REFERENCES Users(Id),
 	[IdGroup] INT NOT NULL CONSTRAINT FK_MessageGroups_Group FOREIGN KEY REFERENCES Groups(Id),
 	[Content] TEXT NOT NULL,
-	[Status] VARCHAR(10) NOT NULL,
+	[Status] INT NOT NULL,
+	[SeenMembers] VARCHAR(100) NOT NULL,
 	[DateTime] DATETIME NOT NULL
 )
